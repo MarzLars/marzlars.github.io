859469cc42b4175d7013dae04e5f6d96b54df8fc
MarzLars <lars_gustav@hotmail.com>
2024-11-29 02:12:08 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const fs = require('fs');
-
 module.exports = {
   distDir: 'out',
   exportTrailingSlash: true,
@@ -18,15 +15,18 @@ module.exports = {
       '/experience': { page: '/experience' },
       '/projects': { page: '/projects' },
       '/404': { page: '/404' }
-    };
+    }
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
+      const fs = require('fs');
       const outDir = path.resolve(__dirname, 'out');
       if (fs.existsSync(outDir)) {
         console.log('The ./out directory exists.');
       } else {
-        console.log('The ./out directory does not exist.');
+        console.error('The ./out directory does not exist.');
+        fs.mkdirSync(outDir);
+        console.log('The ./out directory has been created.');
       }
     }
     return config;
