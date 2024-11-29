105a7eefff9a9d775af33d9ba543d2eeee9ba449
MarzLars <lars_gustav@hotmail.com>
2024-11-29 02:35:40 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,36 +1,36 @@
-const path = require('path');
+const fs = require('fs');
 
 module.exports = {
-    distDir: 'out',
-    assetPrefix: './',
-    basePath: '',
-    trailingSlash: true,
-    exportPathMap: async function (
-        defaultPathMap,
-        { dev, dir, outDir, distDir, buildId }
-      ) {
-        return {
-          '/': { page: '/' },
-          '/about': { page: '/about' },
-          '/contact': { page: '/contact' },
-          '/experience': { page: '/experience' },
-          '/projects': { page: '/projects' },
-          '/404': { page: '/404' }
-        }
-      },
-      // other configurations...
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            const outDir = path.resolve(__dirname, 'out');
-            if (fs.existsSync(outDir)) {
-                console.log('The ./out directory exists.');
-                // Add your renaming logic here if needed
-            } else {
-                console.log('The ./out directory does not exist.');
-                fs.mkdirSync(outDir);
-                console.log('The ./out directory has been created.');
-            }
-        }
-        return config;
-    },
+  distDir: 'out',
+  exportTrailingSlash: true,
+  assetPrefix: './',
+  basePath: '',
+  trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/contact': { page: '/contact' },
+      '/experience': { page: '/experience' },
+      '/projects': { page: '/projects' },
+      '/404': { page: '/404' }
+    };
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const fs = require('fs');
+      const outDir = path.resolve(__dirname, 'out');
+      if (fs.existsSync(outDir)) {
+        console.log('The ./out directory exists.');
+      } else {
+        console.error('The ./out directory does not exist.');
+        fs.mkdirSync(outDir);
+        console.log('The ./out directory has been created.');
+      }
+    }
+    return config;
+  },
 };
